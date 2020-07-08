c2254391ccbe17eaa918336aa52ea36a2f8a2389
curie.yoo <user@AL01406570.local>
2020-07-08 14:32:51 +0900

    feat: add router @ app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import ScheduleFormContainer from './containers/ScheduleFormContainer';
 
-function App () {
-  return <div>My Calendar</div>;
-};
+function App() {
+  return (
+    <div>
+      <Route path='/new' component={ScheduleFormContainer} exact={true} />
+    </div>
+  );
+}
 
 export default App;
