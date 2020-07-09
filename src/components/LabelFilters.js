6ffaff3f624da6e55daf51261ac57bbc262923cd
curie.yoo <user@AL01406570.local>
2020-07-09 14:51:57 +0900

    feat: implement label filter component & move label styles to single style file

---
+++ src/components/LabelFilters.js
@@ -0,0 +1,48 @@
+import React, { useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { applyFilter, removeFilter, resetFilter } from '@/modules/labels';
+import { ColorContainer, LabelWrapper, ListWrapper } from './label-styles';
+
+export default function LabelFilters() {
+  const { labels, labelFilter } = useSelector((state) => state.labels);
+  const dispatch = useDispatch();
+  const defaultColor = '#24bca8';
+
+  const isSelected = (label) => {
+    return labelFilter.includes(label.id);
+  };
+
+  const isAllSelected = useMemo(() => labelFilter.length === labels.length || !labelFilter.length, [
+    labels,
+    labelFilter,
+  ]);
+
+  const handleSelect = (label) => {
+    if (isSelected(label)) dispatch(removeFilter(label.id));
+    else dispatch(applyFilter(label.id));
+  };
+
+  const handleSelectAll = () => {
+    if (!isAllSelected) dispatch(resetFilter());
+  };
+
+  const allButton = (
+    <LabelWrapper onClick={() => handleSelectAll()}>
+      <ColorContainer selected={isAllSelected} empty />
+      <span>All</span>
+    </LabelWrapper>
+  );
+
+  const labelList = labels.map((label) => (
+    <LabelWrapper key={label.id} onClick={() => handleSelect(label)}>
+      <ColorContainer selected={isSelected(label)} color={label.color ?? defaultColor} />
+      <span>{label.title}</span>
+    </LabelWrapper>
+  ));
+  return (
+    <ListWrapper>
+      {allButton} {labelList}
+    </ListWrapper>
+  );
+}
---
+++ src/components/label-styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const ColorContainer = styled.div`
+  background-color: ${(props) => props.color};
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin: 8px;
+  ${({ selected }) => selected && `border: 2px solid gray !important;`}
+  ${({ empty }) => empty && `border: 1px dashed gray;`}
+`;
+
+export const PaletteContainer = styled.div`
+  display: flex;
+  width: 300px;
+  padding: 16px;
+`;
+
+export const TitleInput = styled.input`
+  border: 1px solid #ececec;
+  border-radius: 4px;
+  width: 220px;
+  padding: 8px;
+`;
+
+export const LabelButton = styled.button`
+  padding: 8px;
+  border: 1px solid #ececec;
+  border-radius: 4px;
+`;
+
+export const LabelWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SelectedLabelWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const ListWrapper = styled.div`
+  margin: 8px;
+  display: flex;
+  justify-content: center;
+`;
