3e95d9819d647f3a48c491868a2e8b97d631fc12
curious-curie <curieyoo@snu.ac.kr>
2020-07-09 19:47:20 +0900

    feat: implement 100 count limit of creating schedule at certain date

--- src/containers/ScheduleFormContainer.js
+++ src/containers/ScheduleFormContainer.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
 import ScheduleForm from '@Components/ScheduleForm';
 import { createSchedule } from '../modules/schedules';
-import { withRouter } from 'react-router-dom';
+import { createIntervalArray } from '@/utils/dateHelpers';
 
 function ScheduleFormContainer({ history }) {
   const dispatch = useDispatch();
+  const scheduleCount = useSelector((state) => state.schedules.scheduleCount);
   const { labels } = useSelector((state) => state.labels);
   const handleSubmit = (schedule) => {
-    dispatch(createSchedule(schedule));
+    const includedDates = createIntervalArray([schedule.start, schedule.end]);
+    if (includedDates.some((date) => scheduleCount[date] >= 100)) {
+      alert('100개 이상 일정을 생성할 수 없습니다!');
+    } else {
+      dispatch(createSchedule(schedule));
+    }
     history.push('/');
   };
   return (
