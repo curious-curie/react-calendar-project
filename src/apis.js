4af53104aacb6783621f605e652afd8e9be5297e
curie.yoo <user@AL01406570.local>
2020-07-09 11:33:28 +0900

    refactor: move frequently used date functions to utils

--- src/apis.js
+++ src/apis.js
@@ -1,18 +1,31 @@
+import { isBefore, isAfter, startOfMonth, lastDayOfMonth, eachDayOfInterval, format } from 'date-fns';
+import { hasMonth, createDateArray } from '@/utils/dateHelpers';
 import { store } from './index';
-import {
-  isBefore,
-  isAfter,
-  isSameMonth,
-  addMonths,
-  addDays,
-  startOfMonth,
-  lastDayOfMonth,
-  eachDayOfInterval,
-  format,
-} from 'date-fns';
 
-export const getEventsByMonth = (currentDate) => {
-  const { schedules } = store.getState().schedules;
+// export const getEventsByMonth = (currentDate) => {
+//   const { schedules } = store.getState().schedules;
+//   let newSchedules = [];
+//   schedules.forEach((schedule) => {
+//     if (hasMonth(schedule, currentDate)) {
+//       const start = isBefore(new Date(schedule.startDate), startOfMonth(currentDate))
+//         ? startOfMonth(currentDate)
+//         : new Date(schedule.startDate);
+//       const end = isAfter(new Date(schedule.endDate), lastDayOfMonth(currentDate))
+//         ? lastDayOfMonth(currentDate)
+//         : new Date(schedule.endDate);
+//       if (isBefore(end, start)) return [];
+//       const dateArray = createDateArray({ start, end });
+//       dateArray.forEach((date) => {
+//         const items = newSchedules[date];
+//         newSchedules[date] = items ? [...items, schedule] : [schedule];
+//       });
+//     }
+//   });
+//   return newSchedules;
+// };
+// temporary commented out since not used
+
+export const getSchedulesByMonth = (currentDate, schedules) => {
   let newSchedules = [];
   schedules.forEach((schedule) => {
     if (hasMonth(schedule, currentDate)) {
@@ -23,7 +36,7 @@ export const getEventsByMonth = (currentDate) => {
         ? lastDayOfMonth(currentDate)
         : new Date(schedule.endDate);
       if (isBefore(end, start)) return [];
-      const dateArray = eachDayOfInterval({ start, end }).map((date) => format(date, 'yyyy-MM-dd'));
+      const dateArray = createDateArray({ start, end });
       dateArray.forEach((date) => {
         const items = newSchedules[date];
         newSchedules[date] = items ? [...items, schedule] : [schedule];
@@ -31,20 +44,9 @@ export const getEventsByMonth = (currentDate) => {
     }
   });
   return newSchedules;
-};
+}; //temporary for test
 
 export const getEventByID = (id) => {
   const { schedules } = store.getState().schedules;
   return schedules.find((schedule) => +schedule.id === +id);
-};
-
-const hasMonth = (obj, currentDate) => {
-  const start = new Date(obj.startDate);
-  const end = new Date(obj.endDate);
-  const prevLastDay = lastDayOfMonth(addMonths(currentDate, -1));
-  const nextFirstDay = addDays(lastDayOfMonth(currentDate), 1);
-  const isIncluded =
-    (isSameMonth(start, currentDate) || isBefore(start, nextFirstDay)) &&
-    (isSameMonth(end, currentDate) || isAfter(end, prevLastDay));
-  return isIncluded;
 };
