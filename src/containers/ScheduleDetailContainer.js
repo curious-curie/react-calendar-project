2fe90e6bf1709fb2dd05a6af45d99009d88e96f4
curie.yoo <user@AL01406570.local>
2020-07-09 10:13:48 +0900

    feat: implement edit / delete of single schedule @ detail page 

--- src/containers/ScheduleDetailContainer.js
+++ src/containers/ScheduleDetailContainer.js
@@ -1,21 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import ScheduleForm from '@Components/ScheduleForm';
+import { editSchedule, deleteSchedule } from '../modules/schedules';
+
 import * as api from '../apis';
 
-export default function ScheduleDetailContainer({ match }) {
+function ScheduleDetailContainer({ match, history }) {
   const { id } = match.params;
 
   const [schedule, setSchedule] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
   useEffect(() => {
     const event = api.getEventByID(id);
     if (event?.id) setSchedule({ ...event });
   }, [id]);
 
+  const dispatch = useDispatch();
+
+  const handleEdit = (schedule) => {
+    dispatch(editSchedule(schedule));
+    setIsEditing(false);
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteSchedule(id));
+    history.push('/');
+  };
+
   return (
     <div>
-      {schedule.id && <ScheduleForm presetData={schedule} readOnly />}
-      {/* <button>편집</button> */}
+      {schedule.id && (
+        <>
+          <button onClick={() => setIsEditing(!isEditing)}>수정</button>
+          <button onClick={() => handleDelete()}>삭제</button>
+          <ScheduleForm presetData={schedule} readOnly={!isEditing} handleSubmit={handleEdit} />
+        </>
+      )}
     </div>
   );
-  // TODO: Connect editing
 }
+
+export default withRouter(ScheduleDetailContainer);
