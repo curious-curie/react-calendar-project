f5d4070c843c88ea6db8ecd2bf212b0599db845d
curie.yoo <user@AL01406570.local>
2020-07-09 11:31:29 +0900

    feat: implement label filter related actions and reducers

--- src/modules/schedules.js
+++ src/modules/schedules.js
@@ -2,8 +2,13 @@ const GET_SCHEDULES = 'GET_SCHEDULES';
 const CREATE_SCHEDULE = 'CREATE_SCHEDULE';
 const DELETE_SCHEDULE = 'DELETE_SCHEDULE';
 const EDIT_SCHEDULE = 'EDIT_SCHEDULE';
+
 const GET_LABELS = 'GET_LABELS';
 const CREATE_LABEL = 'CREATE_LABEL';
+
+const APPLY_FILTER = 'APPLY_FILTER';
+const REMOVE_FILTER = 'REMOVE_FILTER';
+const RESET_FILTER = 'RESET_FILTER';
 
 export const getSchedules = () => {
   const schedules = JSON.parse(localStorage.getItem('schedules'));
@@ -53,11 +58,27 @@ const initialState = {
   schedules: [
     {
       endDate: '2020-07-17',
-      id: '1',
+      id: 1,
       label: { id: 1, title: '기본' },
       memo: '1234',
       startDate: '2020-07-14',
       title: 'test123',
+    },
+    {
+      endDate: '2020-08-27',
+      id: 2,
+      label: { id: 2, title: '회사' },
+      memo: '회사일임',
+      startDate: '2020-06-11',
+      title: '회사일',
+    },
+    {
+      endDate: '2020-07-13T13:00',
+      id: 3,
+      label: { id: 2, title: '회사' },
+      memo: '회사일임',
+      startDate: '2020-07-13T13:00',
+      title: '2',
     },
   ],
   labels: [
@@ -71,6 +92,7 @@ const initialState = {
       color: '#c5ebfe',
     },
   ],
+  labelFilter: [],
 };
 
 export default function schedules(state = initialState, action) {
@@ -120,6 +142,22 @@ export default function schedules(state = initialState, action) {
       return {
         ...state,
         labels: newLabels,
+      };
+    case APPLY_FILTER:
+      return {
+        ...state,
+        labelFilter: [...state.labelFilter, action.payload],
+      };
+    case REMOVE_FILTER:
+      const newFilters = labelFilter.filter((label) => label.id !== action.payload.id);
+      return {
+        ...state,
+        labelFilter: newFilters,
+      };
+    case RESET_FILTER:
+      return {
+        ...state,
+        labelFilter: [],
       };
     default:
       return state;
