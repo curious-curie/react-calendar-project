fc5f115405716820a61f871eef402208c21d26ca
curie.yoo <user@AL01406570.local>
2020-07-09 10:35:47 +0900

    fix: fix button titles

--- src/components/LabelPicker.js
+++ src/components/LabelPicker.js
@@ -104,7 +104,7 @@ export default function LabelPicker({ readOnly, handleSelect, selected }) {
       <ListWrapper>
         <TitleInput type="text" name="name" onChange={setTitle} />
         <LabelButton type="button" onClick={handleCreateLabel}>
-          완료!
+          만들기
         </LabelButton>
       </ListWrapper>
     </div>
