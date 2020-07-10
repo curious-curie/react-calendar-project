44688822fa71ea216b7070e5519b09556cdfbf9a
curious-curie <curieyoo@snu.ac.kr>
2020-07-10 14:20:39 +0900

    feat: implement schedule list item component and style schedule list

--- src/components/List/ScheduleList.js
+++ src/components/List/ScheduleList.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import ScheduleListItem from './ScheduleListItem';
+
+const DateWrapper = styled.div`
+  display: flex;
+  padding: 8px;
+`;
+
+const Item = styled.div`
+  flex: 1;
+`;
+
+const EmptyWrapper = styled.div`
+  display: flex;
+  margin-bottom: 8px;
+  border-bottom: 1px solid #ececec;
+  padding: 0 20px 8px;
+  color: #ccc;
+`;
 
 export default function ScheduleList({ dates, schedules }) {
   return (
     <div>
       {dates.map((date) => (
-        <div key={date}>
-          {date}
-          {schedules[date] ? (
-            <ul>
-              {schedules[date].map((schedule) => (
-                <Link key={schedule.id} to={`/${schedule.id}`}>
-                  <li>{schedule.title}</li>
+        <DateWrapper key={date}>
+          <div>{date}</div>
+          <Item>
+            {schedules[date] ? (
+              <div>
+                {schedules[date].map((schedule) => (
+                  <Link key={schedule.id} to={`/${schedule.id}`}>
+                    <ScheduleListItem date={date} schedule={schedule} />
+                  </Link>
+                ))}
+              </div>
+            ) : (
+              <div>
+                <Link to={{ pathname: '/new', search: `?date=${date}` }}>
+                  <EmptyWrapper>새 일정 만들기</EmptyWrapper>
                 </Link>
-              ))}
-            </ul>
-          ) : (
-            <ul>
-              <Link to={{ pathname: '/new', search: `?date=${date}` }}>
-                <li>새 일정 만들기</li>
-              </Link>
-            </ul>
-          )}
-        </div>
+              </div>
+            )}
+          </Item>
+        </DateWrapper>
       ))}
     </div>
   );
