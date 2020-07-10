85668331ef69d159d424c48b5aa08147e37fa739
curious-curie <curieyoo@snu.ac.kr>
2020-07-10 15:25:55 +0900

    feat: use persist-redux to store state @ local Storage

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import { store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
