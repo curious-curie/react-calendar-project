16e5b88c68b4e601f6a5eabc62c4569967a771c0
curious-curie <curieyoo@snu.ac.kr>
2020-07-13 19:29:20 +0900

    style: fix modal style & fix: update repeat end when created schedule is later than that

--- src/containers/ScheduleFormContainer.js
+++ src/containers/ScheduleFormContainer.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-
+import { isAfter } from 'date-fns';
 import ScheduleForm from '@/components/Form/ScheduleForm';
-import { createSchedule, createRepeatedSchedules, createReservation } from '@/modules/schedules';
+import { createSchedule, createRepeatedSchedules, createReservation, updateRepeatEnd } from '@/modules/schedules';
 import { createIntervalArray } from '@/utils/dateHelpers';
 
 function ScheduleFormContainer({ history, location }) {
   const dispatch = useDispatch();
   const scheduleCount = useSelector((state) => state.schedules.scheduleCount);
+  const repeatEnd = useSelector((state) => state.schedules.repeatEnd);
   const defaultDate = new URLSearchParams(location.search).get('date');
   const { labels } = useSelector((state) => state.labels);
 
   const handleSubmit = (schedule) => {
     const includedDates = createIntervalArray([schedule.start, schedule.end]);
+    if (isAfter(schedule.end, repeatEnd)) dispatch(updateRepeatEnd());
     if (includedDates.some((date) => scheduleCount[date] >= 100)) {
       alert('100개 이상 일정을 생성할 수 없습니다!');
     } else {
